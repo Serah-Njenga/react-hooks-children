d35cc87281615ce6409657a5fa2a005ffd81260f
Serah-Njenga <serah35.nn@gmail.com>
2023-04-19 17:03:57 +0300

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -4,16 +4,16 @@ import Container from "./Container";
 function App() {
   return (
     <div className="App">
-      <Container header="Example header" direction="horizontal">
-        <div>This is an example!</div>
-        <div>This is an example!</div>
-        <div>This is an example!</div>
-        <div>This is an example!</div>
-        <div>This is an example!</div>
-        <div>This is an example!</div>
-        <div>This is an example!</div>
+      <Container header="TURTLEs" direction="horizontal">
+        <div>Hello am Dorithy</div>
+        <div>Hello am Dora</div>
+        <div>Hello am Sam</div>
+        <div>Hello  am Wanda</div>
+        <div> Hello am Jeon</div>
+        <div>Hello am Bubbles</div>
+        <div>Hello am Prime</div>
       </Container>
-      <Container header="Example header" textPosition="center">
+      <Container header="TURTlEs" textPosition="center">
         <img
           alt="turtle"
           src="https://pbs.twimg.com/profile_images/3022993403/fdae67d800527328cee7a222f048dbbd.jpeg"
